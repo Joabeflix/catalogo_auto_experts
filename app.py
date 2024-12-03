855193b183aa0383f52a7dbd798bf9114e26ef7b
Joabe Alves <joabealvesyt@gmail.com>
2024-12-03 16:26:47 -0300

    git commit -m "Melhorando o código"

--- app.py
+++ app.py
@@ -1,61 +1,30 @@
 import requests
 import os
 from dotenv import load_dotenv
-from token_gerador import gerar_arquivo_token
+from token_gerador import gerar_arquivo_token, ler_txt_token
 
-print('Inicial')
-
-def ler_txt_token():
-    with open("token.txt", "r", encoding="utf-8") as arquivo:
-        access_token = arquivo.read()
-        return access_token
 
 # token_acesso = get_access_token(client_key_value, client_secret_value)
-def retorno_dados(url_valor):
+def retorno_dados(part_number):
+
     access_token = ler_txt_token()
 
-    url = f'{url_valor}'
+    url = f'https://api.intelliauto.com.br/v1/produtos/partnumber/{part_number}'
     headers = {'accept': 'application/json', 'Authorization': f'Bearer {access_token}'}
     response = requests.get(url, headers=headers)
 
     if response.status_code == 401:
-         print('Gerando um novo token')
          gerar_arquivo_token()
-         print('Novo token gerado')
-         return retorno_dados(url_valor)
-    
-    print('Verificando se o status é 200')
+         return retorno_dados(part_number)
+
     if response.status_code == 200:
-        print('Ok')
         return response
     
-def retorno_veiculos(part_number):
-        url = f'https://api.intelliauto.com.br/v1/produtos/partnumber/{part_number}'
-        data = retorno_dados(url).json()
-        veiculos = data['data'][0]['aplicacoes'][0]['veiculos']
+def filtro_dados_json(part_number):
 
-        codigo_veiculos = [
-            veiculo["codigo"]
-            for veiculo in veiculos]
-        
-        lista_veiculos = []
-        for codigo in codigo_veiculos:
-            url = f'https://api.intelliauto.com.br/v1/veiculos/codigo/{codigo}'
-            data = retorno_dados(url).json()
-            
-            resultado = {
-                "marca": data["marca"],
-                "nome": data["nome"],
-                "modelo": data["modelo"],
-                "anosDeVenda": data["anosDeVenda"]
-            }
-            lista_veiculos.append(resultado)
+        data = retorno_dados(part_number).json()
+        veiculos = data['data'][0]['aplicacoes'][0]['descricaoFrota']
+        return veiculos
 
-            # Transformando os dados em uma única variável formatada
-            dados_formatados = "\n".join([
-                f"{item['marca']} {item['nome']} {item['modelo']} ANO: {', '.join(map(str, item['anosDeVenda']))}"
-                for item in lista_veiculos
-            ])
+print(filtro_dados_json('AP 30460'))
 
-        return dados_formatados
-print(retorno_veiculos('AP 30460'))
--- token_gerador.py
+++ token_gerador.py
@@ -4,9 +4,10 @@ from dotenv import load_dotenv
 import os
 
 # Buscando os dados de autenticação no .env
-load_dotenv()
+load_dotenv() 
 client_key_value = os.getenv('CLIENT_KEY')
 client_secret_value = os.getenv('CLIENT_SECRET')
+
 
 #  Função para retorna o token de acesso
 def get_access_token(client_key, client_secret):
@@ -20,9 +21,25 @@ def get_access_token(client_key, client_secret):
         return token_response.json().get('accessToken')
     return None
 
-def gerar_arquivo_token():
 
+# Função para gerar um aquivo de token
+def gerar_arquivo_token():
     token = get_access_token(client_key_value, client_secret_value)
     with open("token.txt", "w", encoding="utf-8") as arquivo:
         arquivo.write(token)
+
+
+# Função para ler o token no arquivo txt
+def ler_txt_token():
+    try:
+        with open("token.txt", "r", encoding="utf-8") as arquivo:
+            access_token = arquivo.read()
+            return access_token
+        
+# Se não existir o token, executa a função de gerar o token
+# E em seguida executa novamente a função ler_txt_token
+    except FileNotFoundError:
+        gerar_arquivo_token()
+        ler_txt_token()
+
 
