1a04f79bf991bf6a2af3251967f10ec6caa10d6a
Joabe Alves <joabealvesyt@gmail.com>
2024-12-05 17:47:30 -0300

    git commit -m ""

--- tratamento_exel.py
+++ tratamento_exel.py
@@ -8,7 +8,7 @@ class exel():
         pass
 
     def gerar_planilha_padrao(self, local_salvar):
-        colunas = ['Código Produto', 'Nome', 'Marca', 'Aplicação', 'Peso']
+        colunas = ['Código Produto', 'Nome', 'Marca', 'Aplicação', 'Peso', 'Código de barras (EAN)', 'NCM']
         planilha = pd.DataFrame(columns=colunas)
         planilha.to_excel('modelo_planilha.xlsx', index=False)
 
@@ -20,6 +20,8 @@ class exel():
         lista_marca = []
         lista_aplicacao = []
         lista_peso = []
+        lista_codigo_de_barras = []
+        lista_ncm = []
 
         for codigo in planilha['Código Produto']:
             token_manager = TokenGerador()
@@ -33,16 +35,22 @@ class exel():
                 marca = filtro.filtrar_dados(data, "['data'][0]['marca']['nome']")
                 aplicacao = filtro.filtrar_dados(data, "['data'][0]['aplicacoes'][0]['descricaoFrota']")
                 peso = filtro.filtrar_dados(data, "['data'][0]['especificacoes']", "Peso bruto")
-            
+                codigo_de_barras = filtro.filtrar_dados(data, "['data'][0]['especificacoes']", "Código de barras (EAN)")
+                ncm = filtro.filtrar_dados(data, "['data'][0]['especificacoes']", "NCM")
+
             lista_nome.append(nome)
             lista_marca.append(marca)
             lista_aplicacao.append(aplicacao)
             lista_peso.append(peso)
+            lista_codigo_de_barras.append(codigo_de_barras)
+            lista_ncm.append(ncm)
 
         planilha['Nome'] = lista_nome
         planilha['Marca'] = lista_marca
         planilha['Aplicação'] = lista_aplicacao
         planilha['Peso'] = lista_peso
+        planilha['Código de barras (EAN)'] = lista_codigo_de_barras
+        planilha['NCM'] = lista_ncm
 
         planilha.to_excel('modelo_planilha.xlsx', index=False)
             
